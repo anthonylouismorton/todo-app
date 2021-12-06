a87e0a92e24962c589a63308a713d565941a7cd1
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-12-05 20:12:50 -0800

    lab done

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import ToDo from './components/todo/todo.js';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Settings from './components/settings/settings';
+import Login from './components/auth/Login.js';
+import Auth from './components/auth/IsAuthorized.js'
 import './scss/header.scss'
 import './scss/form.scss'
 import './scss/footer.scss'
@@ -12,17 +14,22 @@ function App() {
   return (
       <>
           <Router>
+              <Login/>
+              <Auth>
             <Header />
               <Switch>
                   {/* { Main Route /} */}
                   <Route exact path='/'>
+                      <Auth>
                       <ToDo/>
+                      </Auth>
                   </Route>
                   {/* {/ Route to Settings} */}
                   <Route path='/settings'>
                       <Settings/>
                   </Route>
               </Switch>
+              </Auth>
             <Footer />
           </Router>
       </>
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
 import SettingsProvider from './context/settings';
+import AuthProvider from './context/auth.js';
 
 
 class Main extends React.Component {
   render() {
     return (
       <SettingsProvider>
+        <AuthProvider>
           <App />
+        </AuthProvider>
       </SettingsProvider>
     );
   }
